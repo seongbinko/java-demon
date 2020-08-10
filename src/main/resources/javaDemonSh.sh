227465a347e52845df5851d3c9f8ffae227ac10b
SEONGBIN KO <60464424+seongbinko@users.noreply.github.com>
2020-08-10 15:53:31 +0900

    add sh , jar

---
+++ src/main/resources/javaDemonSh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh"
+echo "=======================Start javaDemon.sh======================="
+java -jar java-demon.jar
+echo "=======================end javaDemon.sh========================="
