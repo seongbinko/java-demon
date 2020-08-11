c67e8c962764f332bc0bcf7e5060b9362a2b0352
SEONGBIN KO <60464424+seongbinko@users.noreply.github.com>
2020-08-11 09:32:35 +0900

    Update App.java
    connection 한번만 하도록 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,14 +22,15 @@ public class App {
 	private final static Logger Log = Logger.getGlobal();
 
 	public static void main(String[] args)  {
+		
+		double beginTime = ((double)System.currentTimeMillis()/1000);
+		
 		Log.setLevel(Level.INFO);
-
-
 		BufferedReader bf = null;
 		try {
 			Log.info("======================= App 작업 수행 ================================");
 
-			File file = new File("resources/data.txt");
+			File file = new File("data.txt");
 			bf = new BufferedReader(new FileReader(file));
 
 			String info;
@@ -38,19 +39,27 @@ public class App {
 			String url = "";
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
+			
+			double startConnectionTime = ((double)System.currentTimeMillis()/1000);
+			Connection con = DriverManager.getConnection(url, "", "");
+			double endConnectionTime = ((double)System.currentTimeMillis()/1000);
+			Log.info("데이터 커넥션 시간: " + (endConnectionTime - startConnectionTime) + "초");
+
+			con.setAutoCommit(false);
+			String insertSql = "INSERT INTO SOMETABLE (?,to_date(?,'YYYY-MM-DD HH24:MI:SS'),?,?,?,?,?,?,?,?,?,nvl(?,'10'),?,to_date(?,'YYYY-MM-DD HH24:MI:SS'),?,?,nvl(to_date(?,'YYYY-MM-DD HH24:MI:SS'),sysdate),to_date(?,'YYYY-MM-DD HH24:MI:SS'),nvl(?,'ADMIN'),?,?,?,nvl(?,'N'),?,?,?,to_number(?),to_date(?,'YYYY-MM-DD HH24:MI:SS'),?)";
+			
+			PreparedStatement pmst = null;
+			// sql구문을 수행하는 역할
+			
 			while((info = bf.readLine()) != null) {
 
 				Log.info("데이터 정보\n" + info);
-
-				Connection con = DriverManager.getConnection(url, "", "");
-				con.setAutoCommit(false);
-				String insertSql = "INSERT INTO TABLE VALUES (?,to_date(?,'YYYY-MM-DD HH24:MI:SS'),?,?,?,?,?,?,?,?,?,nvl(?,'10'),?,to_date(?,'YYYY-MM-DD HH24:MI:SS'),?,?,nvl(to_date(?,'YYYY-MM-DD HH24:MI:SS'),sysdate),to_date(?,'YYYY-MM-DD HH24:MI:SS'),nvl(?,'ADMIN'),?,?,?,nvl(?,'N'),?,?,?,to_number(?),to_date(?,'YYYY-MM-DD HH24:MI:SS'),?)";
-
-				PreparedStatement pmst = con.prepareStatement(insertSql);
+				pmst = con.prepareStatement(insertSql);
 				try {
 					// int limit 인자 0일경우 zero length는 무시, -1(음수)일 경우는 공백도 포함, 양수일시 length의 길이를 제한한다.
 					String[] arr =  info.split(" \\|",-1);
-
+					
+					// pmst의 set beginIndex는 1부터 시작이어서 int i=1로 잡음 arr은 0이 beginIndex
 					for(int i=1; i<=arr.length; i++) {
 						if("".equals(arr[i-1])) {
 							pmst.setNull(i, java.sql.Types.NULL);
@@ -59,29 +68,31 @@ public class App {
 						}
 					}
 					pmst.executeUpdate();
-
 					con.commit();
+					pmst.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 					Log.info("===========================오류발생 rollback===============================");
 					con.rollback();
-
-				} finally {
-					pmst.close();
-					con.close();
-				}
+				} 
 			}
 
+			con.close();
 		} catch (Exception e) {
+			//에러 메세지의 발생 근원지를 찾아서 단계별로 에러를 출력한다
 			e.printStackTrace();
 		} finally {
-			try {
-				if(bf != null) bf.close();
-				Log.info("==================App 작업 완료 =================");
-			} catch (IOException e) {
-				e.printStackTrace();
+			if(bf != null) {
+				try {
+					bf.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					Log.info("=====================data.txt 읽는 중 오류 발생=============================");
+				}
 			}
+			Log.info("==================App 작업 완료 =================");
 		}
-
+		double endTime = ((double)System.currentTimeMillis()/1000);
+		Log.info("총 소요 시간: " + (endTime - beginTime) + "초");
 	}
 }
